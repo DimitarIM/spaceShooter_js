2f5744fa74fbbf5d9e3164b0b37b0d94f3296462
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-01-27 11:11:58 +0100

    loaded new sprites into ctx

--- src/main.js
+++ src/main.js
@@ -2,8 +2,51 @@ import k from "./kaplayCtx";
 import menu from "./scenes/menu"
 import level_1 from "./scenes/level_1"
 
+function loadSprites() {
+//Backgrounds
 k.loadSprite("level_1-bg", "graphics/level_1-bg.png");
+k.loadSprite("level_2-bg", "graphics/level_2-bg.png");
+k.loadSprite("level_3-bg", "graphics/level_3-bg.png");
+k.loadSprite("level_4-bg", "graphics/level_4-bg.png");
 
+//Projectiles
+k.loadSprite("bullet", "graphics/projectiles/bullet.png", {
+    sliceX: 2,
+    sliceY: 1,
+    anims: {
+       move: { from: 0, to: 1, loop: true, speed: 30 } 
+    },
+});
+k.loadSprite("bullet_big", "graphics/projectiles/bullet_big.png", {
+    sliceX: 4,
+    sliceY: 1,
+    anims: {
+       move: { from: 0, to: 3, loop: true, speed: 30 } 
+    },
+});
+k.loadSprite("ray", "graphics/projectiles/ray.png", {
+    sliceX: 4,
+    sliceY: 1,
+    anims: {
+       move: { from: 0, to: 3, loop: true, speed: 30 } 
+    },
+});
+k.loadSprite("torpedo", "graphics/projectiles/torpedo.png", {
+    sliceX: 3,
+    sliceY: 1,
+    anims: {
+       move: { from: 0, to: 2, loop: true, speed: 30 } 
+    },
+});
+k.loadSprite("wave", "graphics/projectiles/wave.png", {
+    sliceX: 6,
+    sliceY: 1,
+    anims: {
+       move: { from: 0, to: 5, loop: true, speed: 30 } 
+    },
+});
+
+//Entities
 k.loadSprite("player", "graphics/player/player.png");
 k.loadSprite("player-engine", "graphics/player/player-engine.png", {
     sliceX: 3,
@@ -22,7 +65,7 @@ k.loadSprite("player-weapon", "graphics/player/player-weapon.png", {
 });
 
 k.loadSprite("asteroid", "graphics/enemies/asteroid.png");
-k.loadSprite("asteroid-explode", "destruction/asteroid-explode.png", {
+k.loadSprite("asteroid-explode", "graphics/destruction/asteroid-explode.png", {
     sliceX: 8,
     sliceY: 1,
     anims: {
@@ -30,10 +73,75 @@ k.loadSprite("asteroid-explode", "destruction/asteroid-explode.png", {
     }
 });
 
+k.loadSprite("fighter", "graphics/enemies/fighter.png");
+k.loadSprite("fighter-explode", "graphics/destruction/fighter-explode.png",{
+    sliceX: 9,
+    sliceY: 1,
+    anims: {
+        destroy: { form: 0, to: 8, loop: false, speed: 30}
+    }
+});
+
+k.loadSprite("frigate", "graphics/enemies/frigate.png");
+k.loadSprite("frigate-explode", "graphics/destruction/frigate-explode.png",{
+    sliceX: 9,
+    sliceY: 1,
+    anims: {
+        destroy: { form: 0, to: 8, loop: false, speed: 30}
+    }
+});
+k.loadSprite("frigate-shield", "graphics/shields/frigate-shield.png",{
+    sliceX: 40,
+    sliceY: 1,
+    anims: {
+        shielding: { form: 0, to: 39, loop: true, speed: 30}
+    }
+});
+
+k.loadSprite("battlecruiser", "graphics/enemies/battlecruiser.png");
+k.loadSprite("battlecruiser-explode", "graphics/destruction/battlecruiser-explode.png",{
+    sliceX: 14,
+    sliceY: 1,
+    anims: {
+        destroy: { form: 0, to: 13, loop: false, speed: 30}
+    }
+});
+
+k.loadSprite("gartagua", "graphics/enemies/gartagua.png");
+k.loadSprite("gartagua-explode", "graphics/destruction/gartagua-explode.png",{
+    sliceX: 12,
+    sliceY: 1,
+    anims: {
+        destroy: { form: 0, to: 11, loop: false, speed: 60}
+    }
+});
+k.loadSprite("gartagua-shield", "graphics/shields/frigate-shield.png",{
+    sliceX: 10,
+    sliceY: 1,
+    anims: {
+        shielding: { form: 0, to: 9, loop: true, speed: 30}
+    }
+});
+
+k.loadSprite("support", "graphics/enemies/support.png");
+k.loadSprite("support-explode", "graphics/destruction/support-explode.png",{
+    sliceX: 10,
+    sliceY: 1,
+    anims: {
+        destroy: { form: 0, to: 9, loop: false, speed: 30}
+    }
+});
+
+//Sounds
 k.loadSound("main-menu_ambience", "sounds/main-menu_ambience.mp3");
 
+//Fonts
 k.loadFont("pixelopolis", "fonts/pixelopolis9000.ttf");
+}
 
+loadSprites();
+
+//Scenes
 k.scene("menu", menu)
 k.scene("level_1", level_1)
 // k.scene("level_2", level_2)
